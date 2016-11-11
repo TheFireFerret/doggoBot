befb91ae2fa1e17fecbab94f0e9b4332cdca9edd
Sami Glasco <samiglasco@gmail.com>
2016-11-11 13:50:54 -0600

    new request?

--- server.js
+++ server.js
@@ -132,30 +132,12 @@ function getImage () {
 	var http = require('http');
 	var url = 'https://api.tumblr.com/v2/tagged?tag=husky&api_key=' + process.env.TUMBLR_ACCESS_KEY;
 
-	var options = {
-	  host: url,
-	  path: ''
-	};
-
-	var req = http.get(options, function(res) {
-	  console.log('STATUS: ' + res.statusCode);
-	  console.log('HEADERS: ' + JSON.stringify(res.headers));
-
-	  // Buffer the body entirely for processing as a whole.
-	  var bodyChunks = [];
-	  res.on('data', function(chunk) {
-	    // You can process streamed parts here...
-	    bodyChunks.push(chunk);
-	  }).on('end', function() {
-	    var body = Buffer.concat(bodyChunks);
-	    console.log('BODY: ' + body);
-	    // ...and/or process the entire body here.
-	  })
-	});
-
-	req.on('error', function(e) {
-	  consloe.log('ERROR: ' + e.message);
-	});
+	var request = require('request');
+	request(url, function (error, response, body) {
+	  if (!error && response.statusCode == 200) {
+	    console.log(body) // Print the google web page.
+	  }
+	})
 
 }
 

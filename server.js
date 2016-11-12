7e09d0fa0ba385aa9a1c7bf5d54457478a8acb28
Sami Glasco <samiglasco@gmail.com>
2016-11-11 19:55:56 -0600

    call img

--- server.js
+++ server.js
@@ -107,7 +107,7 @@ slapp
 
 	// demonstrate returning an attachment...
 	slapp.message('doggos', ['mention', 'direct_message'], (msg) => {
-		// console.log(getImage())
+		getImage()
 		msg.say({
 			text: 'Check out this amazing attachment! :confetti_ball: ',
 			attachments: [{
